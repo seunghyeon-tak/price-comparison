d3347b0baa434c17563c61e26b51e648c07670a8
mason <conficker77@gmail.com>
2025-03-07 00:43:39 +0900

    first commit

---
+++ src/test/java/com/github/seunghyeon_tak/price_comparison/PriceComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.seunghyeon_tak.price_comparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceComparisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
