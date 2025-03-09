2e371380684288dae49698d50ef3e0d2409aa412
mason <conficker77@gmail.com>
2025-03-10 01:44:20 +0900

    [feat] Add Category Entity

---
+++ src/main/java/com/github/seunghyeon_tak/price_comparison/db/domain/CategoryEntity.java
@@ -0,0 +1,22 @@
+package com.github.seunghyeon_tak.price_comparison.db.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "category")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 45)
+    private String name;
+}
