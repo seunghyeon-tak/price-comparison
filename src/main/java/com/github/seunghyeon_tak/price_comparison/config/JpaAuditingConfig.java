55f24b2b05e86ebfa5c301fb23d78bc4a24db00e
mason <conficker77@gmail.com>
2025-03-23 22:49:31 +0900

    [feat] Add user entity created_at and updated_at column

---
+++ src/main/java/com/github/seunghyeon_tak/price_comparison/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package com.github.seunghyeon_tak.price_comparison.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
