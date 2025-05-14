38ad99b8c57e2e187dd59426100cb700c55c0d11
mason <conficker77@gmail.com>
2025-05-14 15:47:56 +0900

    feat : 사용자 상품 찜 삭제 API 추가

--- src/main/java/com/github/seunghyeon_tak/price_comparison/common/exception/response/enums/user/UserFavoritesResponseCode.java
+++ src/main/java/com/github/seunghyeon_tak/price_comparison/common/exception/response/enums/user/UserFavoritesResponseCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum UserFavoritesResponseCode implements ResponseCodeIfs {
     EXISTENCE_IN_PRODUCT(HttpStatus.BAD_REQUEST.value(), 5000, "이미 찜한 상품입니다."),
+    NOT_FOUND_IN_USER_FAVORITES(HttpStatus.BAD_REQUEST.value(), 5001, "사용자 찜 목록에서 찾을 수 없습니다."),
     ;
 
     private final Integer httpStatusCode;
