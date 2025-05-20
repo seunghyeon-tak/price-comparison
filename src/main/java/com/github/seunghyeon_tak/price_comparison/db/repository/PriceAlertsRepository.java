a2bec1f1907ea42ab5b6b724373357f602ce913d
mason <conficker77@gmail.com>
2025-05-21 00:18:21 +0900

    feat : 알림 등록 API 추가

--- src/main/java/com/github/seunghyeon_tak/price_comparison/db/repository/PriceAlertsRepository.java
+++ src/main/java/com/github/seunghyeon_tak/price_comparison/db/repository/PriceAlertsRepository.java
@@ -1,7 +1,13 @@
 package com.github.seunghyeon_tak.price_comparison.db.repository;
 
 import com.github.seunghyeon_tak.price_comparison.db.domain.PriceAlertsEntity;
+import com.github.seunghyeon_tak.price_comparison.db.domain.ProductEntity;
+import com.github.seunghyeon_tak.price_comparison.db.domain.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
 public interface PriceAlertsRepository extends JpaRepository<PriceAlertsEntity, Long> {
+    Optional<PriceAlertsEntity> findByUserAndProductAndTargetPriceAndIsActive(UserEntity user, ProductEntity product, BigDecimal targetPrice, boolean isActive);
 }
