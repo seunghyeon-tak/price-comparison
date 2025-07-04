5693975c22fcb9871dbe12ee041328d72c0d8c0e
mason <conficker77@gmail.com>
2025-07-04 18:18:28 +0900

    test : 운영빌드시 test 코드단에서 flyway 마이그레이션 시도로 인한 애노테이션 추가

--- src/test/java/com/github/seunghyeon_tak/price_comparison/PriceComparisonApplicationTests.java
+++ src/test/java/com/github/seunghyeon_tak/price_comparison/PriceComparisonApplicationTests.java
@@ -2,8 +2,10 @@ package com.github.seunghyeon_tak.price_comparison;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.flyway.enabled=false")
 class PriceComparisonApplicationTests {
 
 	@Test
